863a4986516acee36e4042993984399598d06147
Gladkowski <marcingladkowski@gmail.com>
2018-02-24 19:54:22 +0100

    Install mongoose, add db.dist.file, create Task model

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const mongoose   = require('mongoose');
 const express        = require('express');
 const bodyParser     = require('body-parser');
 const path           = require('path');
+const db             = require('./config/db');
 const app            = express();
 const port = 3000;
 const task = require('./routes/task');
@@ -13,6 +14,8 @@ app.use(express.static(path.join(__dirname, './public')));
 app.use('/jquery', express.static(__dirname + '/node_modules/jquery/dist/'));
 
 app.use('/', task);
+
+mongoose.connect(db.url);
 
 app.listen(port);
 console.log('server starts at 127.0.0.1::' + port);
