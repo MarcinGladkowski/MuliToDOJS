d2448060a3eecdc9f2bc735b8e00b445202aa92b
Gladkowski <marcingladkowski@gmail.com>
2018-03-24 18:28:34 +0100

    working on save task to db

--- public/components/Header.jsx
+++ public/components/Header.jsx
@@ -4,14 +4,32 @@ import React from 'react';
 class Header extends React.Component{
     constructor(){
         super();
+        this.state = {};
+    }
+
+    newTask(event){
+        if(event.key === 'Enter'){
+            console.log(event.target.value);
+        }
+    };
+
+    saveTask(data){
+        fetch('http://localhost:3000/task', {
+            method: 'POST',
+            headers: { 
+              'Accept': 'application/json',
+              'Content-Type':'application/json' },
+            body: JSON.stringify({}),
+          })
+    
     }
 
     render(){
         return(
-                    <div className="header">
-                        <h1>todos</h1>
-                        <input className="new-todo" placeholder="What needs to be done?"/>
-                    </div>            
+            <div className="header">
+                <h1>todos</h1>
+                <input className="new-todo" onKeyPress={event => this.newTask(event)} placeholder="What needs to be done?"/>
+            </div>            
         )
     }
 }
