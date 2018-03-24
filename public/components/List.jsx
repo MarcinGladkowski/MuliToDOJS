b62f55b3b4edac00ad44f92989adbdb4b952e11a
Gladkowski <marcingladkowski@gmail.com>
2018-03-24 11:59:42 +0100

    render task with props

--- public/components/List.jsx
+++ public/components/List.jsx
@@ -1,29 +1,43 @@
 import React from 'react';
+import Task from './Task.jsx';
 
 import '../css/base.css';
 import '../css/index.css';
 
 class List extends React.Component {
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
+        this.state = {
+            error: null,
+            isLoaded: false,
+            tasks: []
+          };
     }
 
+    componentDidMount() {
+        fetch("http://localhost:3000/task")
+          .then(res => res.json())
+          .then(
+            (result) => {
+              this.setState({
+                error: null,
+                isLoaded: true,
+                tasks: result
+              });
+            },
+          )
+      }
+
     render(){
-        return(
-            
+        return( 
                 <section className="main">
-                <input id="toggle-all" className="toggle-all" type="checkbox"/>
-                <label>Mark all as complete</label>
-                <ul className="todo-list">
-                    <li data-id="#" className="">
-                        <div className="view">
-                            <input className="toggle" type="checkbox"/>
-                            <label>Tytuł taska</label>
-                            <button className="destroy"></button>
-                        </div>
-                        <input className="edit"/>
-                    </li> 
-                </ul>
+                    <input id="toggle-all" className="toggle-all" type="checkbox"/>
+                    <label>Mark all as complete</label>
+                        <ul className="todo-list">
+                            {this.state.tasks.map((task, i) => (
+                                <Task key={i} task={task} />
+                            ))}
+                        </ul>
                 </section>
             
         )
--- public/components/Task.jsx
+++ public/components/Task.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
 
 class Task extends React.Component {
-    constructor(){
-        super();
+    constructor(props){
+        super(props);
+        this.state = {task: props.task}
+    }
+
+    componentWillReceiveProps(props){
+        this.setState({task: props.task})
     }
 
     render(){
         return(
-            <div>
-               
-            </div>
+            <li data-id="#" className="">
+                        <div className="view">
+                            <input className="toggle" type="checkbox"/>
+                            <label>{this.state.task.title}</label>
+                            <button className="destroy"></button>
+                        </div>
+                        <input className="edit"/>
+             </li> 
         )
     }
 }
